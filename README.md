7c4ffb92d64c7512f4dbd546ccee074f286fe5de
dodaydream <daydream2013@live.com>
2019-03-30 12:25:34 +0800

    update description

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ This project is a collection of useful scripts in MPI
   - [one-key-rate](https://github.com/MPICP/Scripts/blob/master/siweb/one-key-rate.js) ([raw](https://raw.githubusercontent.com/MPICP/Scripts/master/siweb/one-key-rate.js))
 - Canvas
   - [rename-downloaded-files](https://github.com/MPICP/Scripts/blob/master/canvas/rename-downloaded-files.sh) ([raw](https://raw.githubusercontent.com/MPICP/Scripts/master/canvas/rename-downloaded-files.sh))
+  -  [quiz-autofill](https://github.com/MPICP/Scripts/blob/master/canvas/quiz-autofill.js) ([raw](https://raw.githubusercontent.com/MPICP/Scripts/master/canvas/quiz-autofill.js), [data](https://github.com/MPICP/Scripts/blob/master/canvas/quiz-demo-data.js))
--- canvas/quiz-demo-data.js
+++ canvas/quiz-demo-data.js
@@ -1,3 +1,8 @@
+/**
+ * CUTTING EDGE THIRD EDITION UPPER INTERMEDIATE
+ * STUDENTS'BOOK 
+ * UNIT 2 VOCABULARY LIST
+*/
 window.vocabList = [
 'bad-tempered','majority','minority','track','self-aware','bleep','random','afterwards','amused','awkward','confident','curious','dissatisfied','enthusiastic','frustrated','grumpy','impatient','insecure','panicky','unsociable','upset','gathering','commute','perform','prefix','suffix','inspiration','fascinating','destination','constantly','efficient','reflect','talented','succeed','determined','dishonest','a','series','of','marvellous','disguise','pessimistic','optimistic','critical','existence','take','a','nap','inherit','seeking','psychiatrist','anonymous','miserable','consistently','satisfaction','misery','folk','officially','overcrowding','economist','myth','reality','overwhelming','pro-social','anti-social','aggression','violence','contentment','financial','security','variety','lack','of','exhaustion','criticism','non-conformist','anti-social','intellectual','stimulation','entrepreneur','athlete','companion','interaction','political','party','supportive','choir','beneficial','benefit','giggle','annoy','scare','creep','conduct','survey','rational','exposure','psychologist','alliance','from','with','coordinator','administration','strain','repetitive','obesity','reluctant','to','critical','sophisticated','socialization','virtual','world','posture','outweigh','monitor','favor','favour',
 ]
